602285eff12e6e771a1d6fe3b1e6356ac20eb5cc
LackJaw42 <talhakilic326@gmail.com>
2026-03-05 13:47:16 +0300

    Added test class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+        TestClass test = new TestClass();
+        test.func();
         System.out.println("Hello from Neovim on Arch, running in Docker!");
     }
 }
---
+++ src/TestClass.java
@@ -0,0 +1,5 @@
+public class TestClass {
+    public void func(){
+        System.out.println("TestTest");
+    }
+}
